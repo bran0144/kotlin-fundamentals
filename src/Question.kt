796dee8921deaad4ec2f4a3490a71dfac538922e
bra0144 <bran0144@umn.edu>
2021-06-04 10:23:59 -0500

    try/catch clause

--- src/Question.kt
+++ src/Question.kt
@@ -1,24 +1,34 @@
+import java.lang.NumberFormatException
+
 fun main(args:Array<String>) {
-    val q:Question? = Question()
+    val q:Question = Question()
 
-    q?.Answer = null
-    if(q?.Answer != null) {}
-    q?.Answer = "42"
-    q?.display()
+    q.Answer = null
+    if(q.Answer != null) {}
+    q.Answer = "42"
+    q.display()
 
-    println("The answer to the question ${q?.Question} is ${q?.Answer}")
+    println("The answer to the question ${q.Question} is ${q.Answer}")
 
-    var message = if(q?.Answer == q?.CorrectAnswer) {
+    var message = if(q.Answer == q.CorrectAnswer) {
         "You were correct"
     } else{
         "Try again"
     }
     println(message)
-    q?.printResult()
+    q.printResult()
+
+    val number:Int = try{
+        Integer.parseInt(q.Answer)
+    } catch(e:NumberFormatException) {
+        -1
+    }
+
+    println("Number is $number")
 }
 
 class Question {
-    var Answer:String? = null
+    var Answer:String? = "42"
     val CorrectAnswer = "42"
     val Question: String = "What is the answer to life, the universe, and everything?"
 
