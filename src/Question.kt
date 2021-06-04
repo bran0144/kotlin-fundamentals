b1b7273506d8c6513712df5a9c7456098be64db8
bra0144 <bran0144@umn.edu>
2021-06-04 10:26:27 -0500

    for loop in range

--- src/Question.kt
+++ src/Question.kt
@@ -1,30 +1,34 @@
 import java.lang.NumberFormatException
 
 fun main(args:Array<String>) {
-    val q:Question = Question()
 
-    q.Answer = null
-    if(q.Answer != null) {}
-    q.Answer = "42"
-    q.display()
-
-    println("The answer to the question ${q.Question} is ${q.Answer}")
-
-    var message = if(q.Answer == q.CorrectAnswer) {
-        "You were correct"
-    } else{
-        "Try again"
+    for (i in 1..10) {
+        println(i)
     }
-    println(message)
-    q.printResult()
-
-    val number:Int = try{
-        Integer.parseInt(q.Answer)
-    } catch(e:NumberFormatException) {
-        -1
-    }
-
-    println("Number is $number")
+//    val q:Question = Question()
+//
+//    q.Answer = null
+//    if(q.Answer != null) {}
+//    q.Answer = "42"
+//    q.display()
+//
+//    println("The answer to the question ${q.Question} is ${q.Answer}")
+//
+//    var message = if(q.Answer == q.CorrectAnswer) {
+//        "You were correct"
+//    } else{
+//        "Try again"
+//    }
+//    println(message)
+//    q.printResult()
+//
+//    val number:Int = try{
+//        Integer.parseInt(q.Answer)
+//    } catch(e:NumberFormatException) {
+//        -1
+//    }
+//
+//    println("Number is $number")
 }
 
 class Question {
