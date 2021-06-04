6aab68cac03f501b40e12612e28ae2e072deb093
bra0144 <bran0144@umn.edu>
2021-06-04 14:18:43 -0500

    added an extension function

--- src/com/rsk/Extensions.kt
+++ src/com/rsk/Extensions.kt
@@ -2,10 +2,15 @@ package com.rsk
 
 fun main(args: Array<String>) {
     val text = "With     multiple \t whitespace"
-    println(text)
+    println(replaceMultipleWhiteSpace(text))
+    println(text.replaceMultipleWhiteSpaceEx())
 }
 
 fun replaceMultipleWhiteSpace(value: String) : String {
     var regex = Regex("\\s+")
     return regex.replace(value, " ")
+}
+fun String.replaceMultipleWhiteSpaceEx() : String {
+    var regex = Regex("\\s+")
+    return regex.replace(this, " ")
 }
