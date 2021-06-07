43533e9704b4f46c4fcfbfc305fce04a8a37bbeb
bra0144 <bran0144@umn.edu>
2021-06-07 10:31:00 -0500

    using interfaces

---
+++ src/com/rsk/Types.kt
@@ -0,0 +1,21 @@
+package com.rsk
+
+interface Time {
+    fun setTime(hours: Int, mins: Int = 0, secs: Int = 0)
+    fun setTime(time: KatieTime) = setTime(time.hours)
+}
+interface EndOfTheWorld {
+    fun setTime(time: KatieTime)
+}
+class KatieTime {
+    var hours: Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
+}
+class YetiTime : Time, EndOfTheWorld {
+    override fun setTime(time: KatieTime) {
+        super<Time>.setTime(time)
+        super<EndOfTheWorld>.setTime(time)
+    }
+    override fun setTime(hours: Int, mins: Int, secs: Int) {}
+}
