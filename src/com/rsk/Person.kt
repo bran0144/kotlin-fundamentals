7a1b66bb368b376c1daff21b6ac37a2f83996a9e
bra0144 <bran0144@umn.edu>
2021-06-09 10:56:14 -0500

    playing with constructors

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,19 +1,28 @@
 package com.rsk
 
 fun main(args: Array<String>) {
-    val katie = Student(1)
+    val katie = Student("Katie","Gott", 1)
     print(katie.id)
 }
 
-abstract class Person {
-    var firstName:String = ""
-    var lastName:String = ""
+abstract class Person(var firstName:String, var lastName:String) {
 
     open fun getName() : String = "$firstName $lastName"
     abstract fun getAddress(): String
     }
 
-class Student(val id: Int): Person() {
+class Student(firstName:String, lastName:String, _id: Int): Person(firstName, lastName) {
+    val id : Int
+    var tutor: String
+
+    init {
+        id = _id
+        tutor = ""
+    }
+
+    constructor(firstName: String, lastName: String, _id: Int, tutor:String) : this(firstName, lastName, _id) {
+        this.tutor = tutor
+    }
     override fun getAddress(): String {
         return ""
     }
