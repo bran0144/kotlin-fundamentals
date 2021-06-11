cee139ed23429fd85fd86a4e52da7e93024b0840
bra0144 <bran0144@umn.edu>
2021-06-11 16:05:21 -0500

    using sequences

--- src/com/rsk/Meeting.kt
+++ src/com/rsk/Meeting.kt
@@ -1,5 +1,14 @@
 package com.rsk
 
+fun main(args: Array<String>) : Unit {
+    val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
+    val titles = meetings
+        .asSequence()
+        .filter{it -> it.title.startsWith("C")}
+        .map{m -> m.title}
+
+    for (t in titles) println(t)
+}
 class Meeting(val id: Int, val title: String) {
     val people = listOf(Person1("Sam"), Person1("Alex"))
 }
