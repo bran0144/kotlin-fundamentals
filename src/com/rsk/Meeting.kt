66bc68707d82bf0bf9f4a3c1fc2b9cd76d8f397d
bra0144 <bran0144@umn.edu>
2021-06-11 16:12:16 -0500

    sequences are lazily evaluated

--- src/com/rsk/Meeting.kt
+++ src/com/rsk/Meeting.kt
@@ -1,5 +1,8 @@
 package com.rsk
 
+fun iter (seq: Sequence<String>) {
+    for (t in seq) println(seq)
+}
 fun main(args: Array<String>) : Unit {
     val meetings = listOf(Meeting(1, "Board Meeting"), Meeting(2, "Committee Meeting"))
     val titles = meetings
@@ -7,7 +10,14 @@ fun main(args: Array<String>) : Unit {
         .filter{it -> it.title.startsWith("C")}
         .map{m -> m.title}
 
-    for (t in titles) println(t)
+    iter(titles)
+
+    val title = meetings
+        .asSequence()
+        .map{it.title.toUpperCase()}
+        .find{ it.startsWith("BOARD")}
+
+    println(title)
 }
 class Meeting(val id: Int, val title: String) {
     val people = listOf(Person1("Sam"), Person1("Alex"))
