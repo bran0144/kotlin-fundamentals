2314cbb2277ba4d9eb36c491f025fe3cab4a8042
bra0144 <bran0144@umn.edu>
2021-06-11 13:21:22 -0500

    added companion object

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,30 +1,67 @@
 package com.rsk
 
 fun main(args: Array<String>) {
-    val katie = Student("Katie","Gott", 1)
+    val katie = Student("Katie","Gott", 1, "some tutor")
     print(katie.id)
+
+    Student.createPostGraduate("Simon")
+    Student.createUndergraduate("Kelly")
 }
 
+class Program {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val katie = Student("Katie", "Gott", 1, "some tutor")
+            print(katie.id)
+        }
+    }
+}
 abstract class Person(var firstName:String, var lastName:String) {
 
     open fun getName() : String = "$firstName $lastName"
     abstract fun getAddress(): String
     }
 
-class Student(firstName:String, lastName:String, _id: Int): Person(firstName, lastName) {
-    val id : Int
-    var tutor: String
+open class Student(firstName:String, lastName:String, _id: Int, var tutor:String = ""): Person(firstName, lastName) {
+    val id: Int
 
     init {
         id = _id
-        tutor = ""
     }
 
-    constructor(firstName: String, lastName: String, _id: Int, tutor:String) : this(firstName, lastName, _id) {
-        this.tutor = tutor
+    fun enroll(courseName: String) {
+        val course = Courses.allCourses
+            .filter { it.Title == courseName }
+            .firstOrNull()
     }
+
     override fun getAddress(): String {
         return ""
     }
-    override fun getName() : String{return ""}
+
+    override fun getName(): String {
+        return ""
+    }
+
+    companion object {
+        fun createUndergraduate(name: String): Undergraduate {
+            return Undergraduate(name)
+        }
+
+        fun createPostGraduate(name: String): Graduate {
+            return Graduate(name)
+        }
+    }
+}
+class Undergraduate(firstName: String) : Student(firstName, "", 1) {
+    override fun toXml(item: Student) {
+
+    }
+}
+class Graduate(firstName: String) : Student(firstName, "", 1) {
+}
+
+interface XmlSerializer<T> {
+    fun toXml(item: T)
 }
