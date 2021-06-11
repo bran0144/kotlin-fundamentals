022d71467dc35767c0fe6bbcb25c398d44b2028d
bra0144 <bran0144@umn.edu>
2021-06-11 15:41:17 -0500

    using variables as predicates

--- src/com/rsk/Predicates.kt
+++ src/com/rsk/Predicates.kt
@@ -2,8 +2,17 @@ package com.rsk
 
 fun main(args: Array<String>) : Unit {
     val ints = listOf(1,2,3,4,5)
+    val greaterThan3 = {v:Int -> v > 3}
 
-    val largeInts = ints.all{it > 3}
+    var largeInts = ints.all(greaterThan3)
 
     println(largeInts)
+
+    largeInts = ints.any(greaterThan3)
+
+    println(largeInts)
+
+    var numberOfLargeInts = ints.count(greaterThan3)
+
+    println(numberOfLargeInts)
 }
