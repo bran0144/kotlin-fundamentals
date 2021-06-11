e130ccf966f2ee0b25ca6ed41347ba18e9276099
bra0144 <bran0144@umn.edu>
2021-06-11 15:19:42 -0500

    filter and map sorting

--- src/com/rsk/DataFilter.kt
+++ src/com/rsk/DataFilter.kt
@@ -5,10 +5,23 @@ fun main(args: Array<String>) {
 
     val smallInts = ints.filter{ it < 4 }
 
-    for (i: Int in smallInts) println(i)
+//    for (i: Int in smallInts) println(i)
 
     val squaredInts = ints.map{ it*it }
 
-    for (i: Int in squaredInts) println(i)
+//    for (i: Int in squaredInts) println(i)
+
+    val smallSquaredInts = ints.filter{it < 5}.map{ it*it }
+
+//    for (i: Int in smallSquaredInts) println(i)
+
+    val meetings = listOf(Meeting1(1, "Board Meeting"), Meeting1(2, "Committee Meeting"))
+
+    val titles: List<String> = meetings.
+        filter{it -> it.title.startsWith("C")}.map{m -> m.title}
+
+    for (t in titles) println(t)
 
 }
+
+class Meeting1(val id: Int, val title: String)
