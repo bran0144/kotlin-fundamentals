df092f92f8589b3908b71fe5abf13f2216cf131f
bra0144 <bran0144@umn.edu>
2021-06-14 10:04:58 -0500

    using a SAM constructor

--- src/com/rsk/kotlin/Program.kt
+++ src/com/rsk/kotlin/Program.kt
@@ -1,9 +1,15 @@
 package com.rsk.kotlin
 
+import com.rsk.java.Created
 import com.rsk.java.User
 
 fun main(args: Array<String>){
     var user = User("Katie")
 
-    user.create{println("User $it has been created")}
+    var count = 0
+
+    var eventListener = Created{ println("User $it has been created: ${++count} times")}
+
+    user.create{eventListener}
+    user.create{println("User $it has been created: ${++count} times")}
 }
