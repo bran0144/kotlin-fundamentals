ba3c8b7b2227a3b64d571f7e32375dfbddf1d5a7
bra0144 <bran0144@umn.edu>
2021-06-14 10:29:17 -0500

    different null handling

--- src/com/rsk/kotlin/OrganizerProgram.kt
+++ src/com/rsk/kotlin/OrganizerProgram.kt
@@ -5,11 +5,14 @@ fun main(args: Array<String>) {
     var newMeeting = Meeting()
 
 //    newMeeting = m
+    closeMeeting(m)
     closeMeeting(newMeeting)
 }
 
-fun closeMeeting(m: Meeting): Boolean? {
-    return if (m.canClose) m.close()
+fun closeMeeting(m: Meeting?): Boolean? {
+//    return if (m?.canClose == true) m?.close()
+//    else false
+    return if (m!!.canClose) m.close()
     else false
 }
 class Meeting {
