629b7b589fcc73165134eeccb97720a719558dd4
bra0144 <bran0144@umn.edu>
2021-06-16 11:06:29 -0500

    using let with null references

--- src/com/rsk/kotlin/OrganizerProgram.kt
+++ src/com/rsk/kotlin/OrganizerProgram.kt
@@ -4,11 +4,16 @@ fun main(args: Array<String>) {
     var m:Meeting? = null
     var newMeeting = Meeting()
 
-//    newMeeting = m
+    newMeeting = m ?: Meeting()
     closeMeeting(m)
     closeMeeting(newMeeting)
-}
 
+    m?.let{closeMeetingNonNull(m)}
+}
+fun closeMeetingNonNull(m: Meeting): Boolean? {
+    return if (m.canClose) m.close()
+    else false
+}
 fun closeMeeting(m: Meeting?): Boolean? {
 //    return if (m?.canClose == true) m?.close()
 //    else false
@@ -21,4 +26,12 @@ class Meeting {
     fun close(): Boolean {
         return true
     }
+    fun save(o: Any){
+        val saveable = o as? ISaveable
+        saveable?.save()
+    }
+}
+
+interface ISaveable{
+    fun save()
 }
