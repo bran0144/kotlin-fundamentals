3d6ee4c21e1c2c8bdfc81579db1b34d54277812a
bra0144 <bran0144@umn.edu>
2021-06-17 13:12:40 -0500

    dealing with nullables

--- src/com/rsk/kotlin/Organizer.kt
+++ src/com/rsk/kotlin/Organizer.kt
@@ -4,8 +4,13 @@ import com.rsk.java.OrgMeeting
 
 fun main(args: Array<String>) {
     val m = OrgMeeting()
+//    m.addTitle("Title")
+//    m.addTitle(null)
+//
+//    val title: String? = m.meetingTitle()
+//
+//    println(title)
 
-    val title: String? = m.meetingTitle()
-
+    val title: String = m.titleCanBeNull() ?: "nobody"
     println(title)
 }
