0d126721d3895cfd4b6944c257440a3c4d2c1fd3
bra0144 <bran0144@umn.edu>
2021-06-17 13:17:31 -0500

    using interfaces

--- src/com/rsk/kotlin/Organizer.kt
+++ src/com/rsk/kotlin/Organizer.kt
@@ -1,5 +1,6 @@
 package com.rsk.kotlin
 
+import com.rsk.java.Address
 import com.rsk.java.OrgMeeting
 
 fun main(args: Array<String>) {
@@ -13,4 +14,14 @@ fun main(args: Array<String>) {
 
     val title: String = m.titleCanBeNull() ?: "nobody"
     println(title)
+}
+class HomeAddress : Address {
+    override fun getFirstAddress(): String{
+        return ""
+    }
+}
+class WorkAddress : Address {
+    override fun getFirstAddress(): String? {
+        return ""
+    }
 }
