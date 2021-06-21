0206ed02bf1bb1b13c496e310526403315ca2f9b
bra0144 <bran0144@umn.edu>
2021-06-21 12:40:01 -0500

    using generic classes

--- src/com/rsk/kotlin/Program.kt
+++ src/com/rsk/kotlin/Program.kt
@@ -5,8 +5,17 @@ fun main(args: Array<String>){
     var names = listOf("Kevin", "Alex")
 
     val name = names.itemAt(0)
+
+    val n:Node<Int> = Node(2)
+    println(n.value())
 }
 
 fun <T> List<T>.itemAt(ndx: Int) : T {
     return this[ndx]
+}
+
+class Node<T>(private val item:T) {
+    fun value():T {
+        return item
+    }
 }
