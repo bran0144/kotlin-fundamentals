f2fb84f52be23f0061423a58649d5f81b82037d4
bra0144 <bran0144@umn.edu>
2021-06-21 12:38:34 -0500

    creating generic functions

--- src/com/rsk/kotlin/Program.kt
+++ src/com/rsk/kotlin/Program.kt
@@ -1,15 +1,12 @@
 package com.rsk.kotlin
 
-import com.rsk.java.Created
-import com.rsk.java.User
 
 fun main(args: Array<String>){
-    var user = User("Katie")
+    var names = listOf("Kevin", "Alex")
 
-    var count = 0
+    val name = names.itemAt(0)
+}
 
-    var eventListener = Created{ println("User $it has been created: ${++count} times")}
-
-    user.create{eventListener}
-    user.create{println("User $it has been created: ${++count} times")}
+fun <T> List<T>.itemAt(ndx: Int) : T {
+    return this[ndx]
 }
